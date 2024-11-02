2018ea52647e8506df8e00bb6f8ac6e96a851deb
AlyAbdelrahman <aly.m.abdelrahman@gmail.com>
2024-11-02 15:37:48 +0200

    adj Navbar HTML hyderation

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,12 +7,12 @@ export default function Navbar() {
     return (
         <nav className="shadow-sm sticky top-0 left-0 bg-white">
             <div className="h-[80px] w-full flex justify-between items-center px-3 max-w-7xl mx-auto">
-                <p className="flex items-center justify-center gap-2">
+                <div className="flex items-center justify-center gap-2">
                     <h2 className="text-gray-500 text-3xl">
                         weather
                     </h2>
                     <MdSunny className='text-yellow-300 text-3xl mt-1'/>
-                </p>
+                </div>
                 {/* */}
                 <Searchbox/>
             </div>
