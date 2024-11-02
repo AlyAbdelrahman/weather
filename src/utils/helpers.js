9d2bd31826d5ab6f556d8a5a5181e41e09b12894
AlyAbdelrahman <aly.m.abdelrahman@gmail.com>
2024-11-02 20:29:11 +0200

    adding day/night selector helper function

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -27,3 +27,12 @@ export function convertKelvinToCelsius(tempInKelvin) {
     const tempInCelsius = tempInKelvin - 273.15;
     return Math.floor(tempInCelsius);
   }
+
+
+  export function getDayOrNightIcon( iconName,dateTimeString) {
+    const hours = new Date(dateTimeString).getHours(); 
+  
+    const isDayTime = hours >= 6 && hours < 18; 
+  
+    return isDayTime ? iconName.replace(/.$/, "d") : iconName.replace(/.$/, "n");
+  }
